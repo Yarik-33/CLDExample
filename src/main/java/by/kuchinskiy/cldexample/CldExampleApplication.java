327cc7f015ae663b5e1f07966b3f0d84c3622e7f
Yarik-33 <yarik.kuchinskiy@gmail.com>
2023-03-06 19:45:05 +0300

    init commit

---
+++ src/main/java/by/kuchinskiy/cldexample/CldExampleApplication.java
@@ -0,0 +1,13 @@
+package by.kuchinskiy.cldexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CldExampleApplication.class, args);
+	}
+
+}
